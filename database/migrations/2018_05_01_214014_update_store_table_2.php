10863a2efd99b023a19e3b641bbc1b783e261744
adrianhartanto97 <adrian.shenzryu@gmail.com>
2018-05-01 21:43:23 +0700

    Update Store Table

--- database/migrations/2018_05_01_214014_update_store_table_2.php
+++ database/migrations/2018_05_01_214014_update_store_table_2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateStoreTable extends Migration
+class UpdateStoreTable2 extends Migration
 {
     /**
      * Run the migrations.
