32653bb0133f434c5e0b6b4226de83251f25f99c
KelvinChen1996 <kelvin_chen96@ymail.com>
2018-07-04 20:10:52 +0700

    Store Detail on progress

--- resources/views/pages/store_detail.blade.php
+++ resources/views/pages/store_detail.blade.php
@@ -4,6 +4,11 @@
     {{ HTML::style('public/global/plugins/fancybox/source/jquery.fancybox.css') }}
     {{ HTML::style('public/star-rating-svg-master/src/css/star-rating-svg.css') }}
     {{ HTML::style('public/global/plugins/bootstrap-touchspin/bootstrap.touchspin.css') }}
+     {{ HTML::style('public/global/plugins/ion.rangeslider/css/ion.rangeSlider.css')}}
+    {{ HTML::style('public/global/plugins/ion.rangeslider/css/ion.rangeSlider.skinFlat.css')}}
+    {{ HTML::style('public/global/plugins/nouislider/nouislider.min.css')}}
+    {{ HTML::style('public/global/plugins/nouislider/nouislider.pips.css')}}
+    {{ HTML::style('public/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css')}}
 
     <style>
     </style>
@@ -28,82 +33,7 @@
                     @endif
 
                     
-                    <div class="row" id="product_info">
-                        <div class="col-md-8">
-                             <div class="col-md-12">
-                                <div class = "portlet box grey-salsa">
-                                    <div class="portlet-title">
-                                        <div class="caption">
-                                            <div class="row" >
-                                                <div class="col-md-4">
-                                                    <img alt="" width="100%" class="img-square" src="{{asset('/public/storage').'/'.$product_detail->store_info->photo}}" />
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <h3>{{$product_detail->store_info->name}}</h3>
-                                                    <h4>{{$product_detail->store_info->city_name}}</h4>
-                                                    
-
-                                                    @if($product_detail->wishlist_status=="true")
-                                                        <form method="POST" action="{{ action('Web_Controller\App2Controller@delete_from_wishlist') }}"  id="form3">
-                                                            {{ csrf_field() }}
-                                                            <div class="form-body">
-                                                                <input type="hidden" name="product_id" value="{{$product_detail->product_info->product_id}}">
-                                                            </div>
-                                                        </form>
-                                                        <button  type="submit" class="btn blue" form="form3" width="200px"  @if($login_info->login_status == false) disabled @endif>Remove from Favorite</button>                                
-                                                    @else
-                                                        <form method="POST" action="{{ action('Web_Controller\App2Controller@add_to_wishlist') }}"  id="form2">
-                                                            {{ csrf_field() }}
-                                                            <div class="form-body">
-                                                                <input type="hidden" name="product_id" value="{{$product_detail->product_info->product_id}}">
-                                                            </div>
-                                                        </form>
-                                                        <button  type="submit" class="btn blue" form="form2"  @if($login_info->login_status == false) disabled @endif>Add to Favorite </button>
-                                                     @endif
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                           
-
-                            
-                        </div>
-                        <div class="col-md-4">
-                             <div class="col-md-12">
-                                <div class = "portlet box grey-salsa">
-                                    <div class="portlet-title">
-                                        
-                                    </div>
-
-                                    <div class="portlet-body" id="store_info">
-                                        <div class="row">
-                                            <div class="col-md-6" style="text-align:center;border-right: 2px solid black;">
-                                                <p>
-                                                    <span style="font-size:20px;">{{$product_detail->store_info->sold}}</span>
-                                                    <br>
-                                                    Sold Products
-                                                </p>
-                                            </div>
-                                            <div class="col-md-6" style="text-align:center">
-                                                <p>
-                                                    <span style="font-size:20px;">{{$product_detail->store_info->transaction}}</span>
-                                                    <br>
-                                                    Total Transaction
-                                                </p>
-                                            </div>
-                                        </div>
-                                       <div class="row" style ="margin-top:20px;">
-                                            <div class="col-md-12" style="text-align:center">
-                                                <div class="my-rating" data-rating="{{$product_detail->product_info->rating}}"></div>
-                                          </div> <br>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                   
                     <div class="row" style="margin-top:50px;">
                             <div class="col-md-8  porlet box grey-salsa" style="border:2px solid black;height: 32%;background-image:url('asset/storage/Product/photo/1_photo.jpg')">
                              <div class="row" >
@@ -354,16 +284,51 @@
     {{HTML::script('public/global/plugins/fuelux/js/spinner.min.js')}}
     {{HTML::script('public/global/plugins/bootstrap-touchspin/bootstrap.touchspin.js')}}
     <!--END PAGE LEVEL PLUGINS-->
+        {{HTML::script('public/global/plugins/ion.rangeslider/js/ion.rangeSlider.min.js')}}
+        {{HTML::script('public/pages/scripts/components-ion-sliders.min.js')}}
+        {{HTML::script('public/global/plugins/nouislider/nouislider.min.js')}}
+        {{HTML::script('public/global/plugins/nouislider/wNumb.min.js')}}
+        {{HTML::script('public/global/plugins/nouislider/nouislider.min.js')}}
+         {{HTML::script('public/global/plugins/jquery.pulsate.min.js')}}
+        {{HTML::script('public/global/plugins/jquery-bootpag/jquery.bootpag.min.js')}}
+        {{HTML::script('public/global/plugins/holder.js')}}
+        {{HTML::script('public/pages/scripts/ui-general.min.js')}}
 
     <!--BEGIN PAGE LEVEL SCRIPTS-->
     <!--END PAGE LEVEL SCRIPTS-->
     <script>
+       
+    
+
         $( document ).ready(function() {
             $(".my-rating").starRating({
                 starSize: 25,
                 readOnly: true,   
             });
-        });    
+        });
+
+        $("#range_25").ionRangeSlider({
+            type: "double",
+            min: 1,
+            max: 5,
+            from: 1,
+            to: 5,
+            hide_min_max: true,
+            hide_from_to: false,
+            grid: false
+        });
+
+         $("#range_26").ionRangeSlider({
+            type: "double",
+            min: 10000,
+            max: 50000000,
+            from: 10000,
+            to: 50000000,
+            hide_min_max: true,
+            hide_from_to: false,
+            grid: false
+        });
+
     </script>
 
 
